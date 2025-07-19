0ed5fe74b98375e07de8ed732f32c2afe0e1b269
andersonkaiti <anderkaiti@gmail.com>
2025-07-19 19:16:25 -0300

    :sparkles: feat(queue): integrate SQS for meal processing in file upload handler

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   DATABASE_URL: z.url().startsWith('postgresql'),
   JWT_SECRET: z.string(),
   BUCKET_NAME: z.string(),
+  MEALS_QUEUE_URL: z.url(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/functions/file-uploaded-event.ts
+++ src/functions/file-uploaded-event.ts
@@ -1,5 +1,17 @@
+import { SendMessageCommand } from '@aws-sdk/client-sqs'
 import type { S3Event } from 'aws-lambda'
+import { sqsClient } from '../clients/sqs-client'
+import { env } from '../config/env'
 
 export async function handler(event: S3Event) {
-  await Promise.all(event.Records.map((record) => record.s3.object.key))
+  await Promise.all(
+    event.Records.map(async (record) => {
+      const command = new SendMessageCommand({
+        QueueUrl: env.MEALS_QUEUE_URL,
+        MessageBody: JSON.stringify({ fileKey: record.s3.object.key }),
+      })
+
+      await sqsClient.send(command)
+    })
+  )
 }
